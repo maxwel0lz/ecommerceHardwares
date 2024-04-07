9886931c7e95e0b2cdbe9b6e882abac2a4790a27
maxwel0lz <134058339+maxwel0lz@users.noreply.github.com>
2024-04-07 11:59:24 -0300

    Autenticação  De cadastro 100%

--- scripts/iteracao.js
+++ scripts/iteracao.js
@@ -5,7 +5,7 @@ const senha = document.querySelector("#iSenha")
 const confsenha = document.querySelector("#iConfSenha")
 const btnCriar = document.querySelector("#btnCriar")
     
-
+                 //FUNÇÕS DOS IMPUTS
 function verificaBasico(idTag){
     const item = document.querySelector(`${idTag}`)
     if(item.value == ''){
@@ -17,6 +17,27 @@ function verificaBasico(idTag){
         item.classList.add("is-valid")   
     }
 }
+function verificaSenha(idTag) {
+    const item = document.querySelector(`${idTag}`)
+    if(item.value.length < 8){
+        item.classList.remove('is-valid')
+        item.classList.add("is-invalid")
+    }                                                   //FUNÇÃO PARA VERIFICAR SENHA SE < 8 = FALSE
+    else if(!item.classList.contains('is-valid')){
+        item.classList.remove('is-invalid')
+        item.classList.add("is-valid")  
+    }
+}
+function ativarBotao() {
+    if (nome.classList.contains('is-valid') && sobrenome.classList.contains('is-valid') && cpf.classList.contains('is-valid') && senha.classList.contains('is-valid') && confsenha.classList.contains('is-valid')) {
+        btnCriar.disabled = false
+    }
+    if (nome.classList.contains('is-invalid') || sobrenome.classList.contains('is-invalid') || cpf.classList.contains('is-invalid') || senha.classList.contains('is-invalid') || confsenha.classList.contains('is-invalid')) {
+        btnCriar.disabled = true
+    }                                                   //FUNÇÃO PARA ATIVAR BOTAO CASO TODOS OS INPUTS ESTIVEREM VALIDOS
+}
+
+                //EVENTO DE CADA INPUT
 document.querySelector('#iCpf').addEventListener("input", function() {
     const item = document.querySelector('#iCpf')
     console.log(item.value.length);
@@ -34,10 +55,10 @@ document.querySelector('#iCpf').addEventListener("keydown", function(event) {
     // Obter o código da tecla pressionada
     let key = event.keyCode;
     // Desativar letras (códigos ASCII 65-90 e 97-122)
-    if ((key >= 60 || key <= 90) || (key >= 106 && key <= 200)) {
+    if ((key >= 60 && key <= 90) || (key >= 106 && key <= 200)) {
         event.preventDefault();
     }
-    //ativarBotao()
+    ativarBotao()
 })
 document.querySelector("#iNome").addEventListener('input', function(){
     verificaBasico("#iNome")
@@ -48,38 +69,16 @@ document.querySelector("#iSobrenome").addEventListener('input', function(){
     verificaBasico("#iSobrenome")
     ativarBotao()
 })
-
-function verificaSenha(idTag) {
-    const item = document.querySelector(`${idTag}`)
-    if(item.value.length < 8){
-        item.classList.remove('is-valid')
-        item.classList.add("is-invalid")
-    }                                                   //FUNÇÃO PARA VERIFICAR SENHA SE < 8 = FALSE
-    else if(!item.classList.contains('is-valid')){
-        item.classList.remove('is-invalid')
-        item.classList.add("is-valid")  
-    }
-}
-
 document.querySelector("#iSenha").addEventListener('input',function(){  
     verificaSenha('#iSenha')
     ativarBotao()
 })
-
 document.querySelector("#iConfSenha").addEventListener('input', function(){
     verificaSenha('#iConfSenha')
     ativarBotao()
 })
 
-function ativarBotao() {
-    console.log('ativo');
-    if (nome.classList.contains('is-valid') && sobrenome.classList.contains('is-valid') && cpf.classList.contains('is-valid') && senha.classList.contains('is-valid') && confsenha.classList.contains('is-valid')) {
-        btnCriar.disabled = false
-    }
-    if (nome.classList.contains('is-invalid') || sobrenome.classList.contains('is-invalid') || cpf.classList.contains('is-invalid') || senha.classList.contains('is-invalid') || confsenha.classList.contains('is-invalid')) {
-        btnCriar.disabled = true
-    }
-}
+
 /*document.querySelector('#btnCriar').addEventListener('input', function(){
     ativarBotao()
 })*/
