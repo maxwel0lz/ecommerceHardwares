670bd487fc2abf1b2ea4af0aa84651b6fd8f7341
maxwel0lz <134058339+maxwel0lz@users.noreply.github.com>
2024-05-18 13:45:40 -0300

    Cadastro pronto

--- controller/cadastrar_controller.php
+++ controller/cadastrar_controller.php
@@ -1,28 +1,56 @@
 <?php
-    function existUsers($cpf , $email){
+session_start();
+include '../data/connectionBD.php';
+$conn = conectBD();
 
+ if($_SERVER["REQUEST_METHOD"]== "POST"){
+    $iNome = $_POST["iNome"];
+    $iSobrenome = $_POST["iSobrenome"];
+    $iCpf = $_POST["iCpf"];
+    $iEmail = $_POST["iEmail"];
+    $iSenha = $_POST["iSenha"];
+    $iConfSenha = $_POST["iConfSenha"];
     }
-    function cadUser(){
-        include '../data/connectionBD.php';
-        if($_SERVER["REQUEST_METHOD"]== "POST"){
-            $iNome = $_POST["iNome"];
-            $iSobrenome = $_POST["iSobrenome"];
-            $iCpf = $_POST["iCpf"];
-            $iEmail = $_POST["iEmail"];
-            $iSenha = $_POST["iSenha"];
-            $iConfSenha = $_POST["iConfSenha"];
+
+    function existUsers($conn ,$cpf , $email , ){
+    $sql = "SELECT * FROM usuario ";
+    //EXECUTANDO QUERY
+    $result = $conn->query($sql);
+
+    //ENQUANTO HOVER RESULTADO PROUCURE DADOS IGUAS OS INPUTS
+    $reposta = true;
+    while($row = $result->fetch_assoc()){
+        if($row['email_user'] == $email){
+            $reposta = false;
+            $_SESSION['error_menssage'] = "email ja existe!";
+            header("Location: ../pages/cadastrar.php");
+            exit;
         }
+        elseif($row['cpf_user'] == $cpf){
+            $reposta = false;
+            $_SESSION['error_menssage'] = "CPF ja existe!";
+            header("Location: ../pages/cadastrar.php");
+            exit; 
+        }
+    }
+    if ($reposta) {
+        cadUser();
+    }
+}
+existUsers($conn ,$iCpf, $iEmail );
+    function cadUser(){
+        global $iConfSenha ,$iNome,$iSobrenome,$iCpf,$iEmail,$iSenha;
+        
         if ($iSenha == $iConfSenha) {
             $conn = conectBD();
             $sql = "INSERT INTO usuario ( nome_user, sobrenome_user, cpf_user, email_user, senha_user) VALUES ('$iNome', '$iSobrenome','$iCpf','$iEmail','$iSenha')";
-    
+            
             if (mysqli_query($conn, $sql)) {
-                echo "Novo registro inserido com sucesso!";
+                header("location: ../index.html");
             } else {
-                echo "Erro ao inserir o registro: " . mysqli_error($conexao);
+                echo "Erro ao inserir o registro: " . mysqli_error($conn);
             }
         }
-        $conn->close();
     }
-    cadUser();
-?>
+    $conn->close();
+    ?>
