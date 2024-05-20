8d1b58ccf20a77b2f4c8b2bec9b724c8d0859d50
maxwel0lz <134058339+maxwel0lz@users.noreply.github.com>
2024-05-19 21:36:31 -0300

    Ajustes

--- scripts/produtos.js
+++ scripts/produtos.js
@@ -90,7 +90,7 @@ function criarViewProduto(id,titulo,desc,imagem) {
 
     const btnComprar = document.createElement('a')
     btnComprar.id = "bntcomprar"
-    btnComprar.href = '#'
+    btnComprar.setAttribute('name' , `${id}`)
     btnComprar.classList = 'link-underline link-underline-opacity-0'
     btnComprar.innerHTML = 'Comprar <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16"><path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/></svg>'
     div2.appendChild(btnComprar)
@@ -113,5 +113,13 @@ btnComprar.addEventListener('click', function(event){
     const iconCart = document.querySelector('#contadorBola')
     
 })
+
+let cout = 0;
+function addCarrinho() {
+    const id = document.querySelector("#btnComprer") 
+    cout += 1
+    console.log(id);
+
+}
 
 
